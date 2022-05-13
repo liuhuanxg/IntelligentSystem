ec8035c4476239a3dcf1bb6fe0c7428d4bd47d2e
devinhliu <devinhliu@tencent.com>
2022-05-13 23:00:23 +0800

    [feature]:更新hbase连接工具

---
+++ utils/parse_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+File Name: parse_file.py
+Author: devinhliu
+Mail: devinhliu@tencent.com
+Created Time: 2022/5/13 18:18
+"""
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
